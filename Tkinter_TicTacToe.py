a638496eae414b4c0e7e04f977903a33d245a8bf
Vaishnavi Kesharinath Shirsekar <99735605+vaishshirsekar31@users.noreply.github.com>
2024-04-27 17:54:40 -0400

    Add files via upload

--- Tkinter_TicTacToe.py
+++ Tkinter_TicTacToe.py
@@ -1,3 +1,6 @@
+import os
+os.environ['TK_SILENCE_DEPRECATION'] = '1'
+
 from tkinter import*
 from tkinter import ttk
 from tkinter import messagebox
@@ -97,53 +100,50 @@ def SetLayout(id,PlayerSymbol):
 
 
 def CheckWinner():
-    Winner=-1
-    if(( 1 in p1) and (2 in p1) and (3 in p1)):
-        Winner=1
-    if (( 1 in p2) and (2 in p2) and (3 in p2)):
-        Winner = 2
-
-    if (( 4 in p1) and (5 in p1) and (6 in p1)):
+    Winner = -1
+    if((1 in p1) and (2 in p1) and (3 in p1)):
         Winner = 1
-    if (( 4 in p2) and (5 in p2) and (6 in p2)):
+    if((1 in p2) and (2 in p2) and (3 in p2)):
         Winner = 2
 
-    if (( 7 in p1) and (8 in p1) and (9 in p1)):
+    if((4 in p1) and (5 in p1) and (6 in p1)):
         Winner = 1
-    if (( 7 in p2) and (8 in p2) and (9 in p2)):
+    if((4 in p2) and (5 in p2) and (6 in p2)):
         Winner = 2
 
-    if (( 1 in p1) and (4 in p1) and (7 in p1)):
+    if((7 in p1) and (8 in p1) and (9 in p1)):
         Winner = 1
-    if (( 1 in p2) and (4 in p2) and (7 in p2)):
+    if((7 in p2) and (8 in p2) and (9 in p2)):
         Winner = 2
 
-    if (( 2 in p1) and (5 in p1) and (8 in p1)):
+    if((1 in p1) and (4 in p1) and (7 in p1)):
         Winner = 1
-    if (( 2 in p2) and (5 in p2) and (8 in p2)):
+    if((1 in p2) and (4 in p2) and (7 in p2)):
         Winner = 2
 
-    if (( 3 in p1) and (6 in p1) and (9 in p1)):
+    if((2 in p1) and (5 in p1) and (8 in p1)):
         Winner = 1
-    if (( 3 in p2) and (6 in p2) and (9 in p2)):
+    if((2 in p2) and (5 in p2) and (8 in p2)):
         Winner = 2
-    if ((1 in p1) and (5 in p1) and (9 in p1)):
-    Winner = 1
-if ((1 in p2) and (5 in p2) and (9 in p2)):
-    Winner = 2
-if ((3 in p1) and (5 in p1) and (7 in p1)):
-    Winner = 1
-if ((3 in p2) and (5 in p2) and (7 in p2)):
-    Winner = 2
 
+    if((3 in p1) and (6 in p1) and (9 in p1)):
+        Winner = 1
+    if((3 in p2) and (6 in p2) and (9 in p2)):
+        Winner = 2
 
+    if((1 in p1) and (5 in p1) and (9 in p1)):
+        Winner = 1
+    if((1 in p2) and (5 in p2) and (9 in p2)):
+        Winner = 2
 
-    if Winner==1:
+    if((3 in p1) and (5 in p1) and (7 in p1)):
+        Winner = 1
+    if((3 in p2) and (5 in p2) and (7 in p2)):
+        Winner = 2
+
+    if Winner == 1:
         messagebox.showinfo(title="Congratulations", message="Player 1 is the winner")
-    elif Winner==2:
+    elif Winner == 2:
         messagebox.showinfo(title="Congratulations", message="Player 2 is the winner")
 
-
-
-root.mainloop()
 
